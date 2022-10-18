666e7b188a7493b0ac6bb86b19ba5022467d7d1e
Martin Franke <Martin.Franke@cas.de>
2022-10-18 23:16:57 +0200

    Add more tests to TestNumber

--- test_number.py
+++ test_number.py
@@ -1,5 +1,6 @@
 """Unit tests to characterize python numbers"""
 
+import math
 import pytest
 
 
@@ -30,5 +31,65 @@ class TestNumber:
         assert -0.3j
         assert not 0
 
+    def test_integer_division(self):
+        assert 7 // 3 == 2
+
+    def test_power(self):
+        assert 3 ** 4 == 81
+
+    def test_round(self):
+        assert round(3.1415926536, 2) == 3.14
+
+    def test_abs(self):
+        assert abs(-15) == 15
+
     def test_divmod(self):
         assert divmod(23, -7) == (-4, -5)
+
+    def test_bin(self):
+        assert bin(27) == '0b11011'
+
+    def test_hex(self):
+        assert hex(27) == '0x1b'
+
+    def test_oct(self):
+        assert oct(27) == '0o33'
+
+    def test_float(self):
+        assert float(3) == 3.0
+
+    def test_int(self):
+        assert int(3.14) == 3
+
+    def test_bool(self):
+        assert bool(3)
+
+    def test_int_from_base(self):
+        assert int('11', 9) == 10
+
+    def test_pow_neg_mod(self):
+        assert pow(3, -2, 7) == 4
+
+    def test_bit_length(self):
+        assert (6).bit_length() == 3
+
+    def test_bit_count(self):
+        assert (6).bit_count() == 2
+
+    def test_trunc(self):
+        assert math.trunc(-2.718) == -2
+
+    def test_floor(self):
+        assert math.floor(2.718) == 2
+
+    def test_ceil(self):
+        assert math.ceil(3.142) == 4
+
+    def test_as_integer_ratio(self):
+        assert 1.25.as_integer_ratio() == (5, 4)
+
+    def test_is_integer(self):
+        assert 7.0.is_integer()
+
+    def test_none_false(self):
+        assert not None
